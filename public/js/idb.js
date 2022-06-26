a460a21872008bc3d84619bace437a5591d6c2ea
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-06-26 12:27:48 -0400

    added idb.js

---
+++ public/js/idb.js
@@ -0,0 +1,8 @@
+let db;
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true });
+}
